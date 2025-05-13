8ebb0f0be7d808ffa0fd1fdb04d088643d2fc0ee
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2025-05-13 12:28:20 +0300

    Create FieldDistortion.jsx

---
+++ src/FieldDistortion.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import './FieldDistortion.css';
+
+function FieldDistortion() {
+  const [position, setPosition] = useState({ x: window.innerWidth / 2, y: window.innerHeight / 2 });
+
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => window.removeEventListener('mousemove', handleMouseMove);
+  }, []);
+
+  return (
+    <div
+      className="distortion-ring"
+      style={{
+        left: `${position.x}px`,
+        top: `${position.y}px`
+      }}
+    ></div>
+  );
+}
+
+export default FieldDistortion;
